70fa5b42b61f290bfc5edc562980409cd4c448df
James <68920695+jamesIVdeveloper@users.noreply.github.com>
2021-02-02 13:30:37 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./Search/Search";
+import BookList from "./BooksList/BooksList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    books: [],
+    query: "",
+    printType: "all",
+    filter: "",
+  };
+
+  searchBooks = () => {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}&printType=${this.state.printType}`;
+
+    if (this.state.filter !== "") {
+      url += `&filter=${this.state.filter}`;
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => this.setState({ books: res.items ? res.items : [] }));
+    console.log("searching...");
+  };
+
+  handleChange = (key, value) => {
+    this.setState({
+      [key]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.searchBooks();
+  };
+
+  componentDidMount() {
+    this.searchBooks();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Google Book Search</h1>
+        <Search
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <p>We found {this.state.books.length} results.</p>
+        <BookList books={this.state.books} />
+      </div>
+    );
+  }
 }
-
-export default App;
