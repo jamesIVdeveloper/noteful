ffa2977de9f0a24cc42b20a39f17ff243648959c
James <68920695+jamesIVdeveloper@users.noreply.github.com>
2021-02-02 13:56:58 -0600

    Add files via upload

---
+++ src/Note/Note.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Note.css";
+
+export default function Note(props) {
+  return (
+    <div className="Note">
+      <h2 className="Note__title">
+        <Link to={`/note/${props.id}`}>{props.name}</Link>
+      </h2>
+      <button className="Note__delete" type="button">
+        <FontAwesomeIcon icon="trash-alt" /> remove
+      </button>
+      <div className="Note__dates">
+        <div className="Note__dates-modified">
+          Modified{" "}
+          <span className="Date">
+            {new Date(props.modified).toLocaleDateString()}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
